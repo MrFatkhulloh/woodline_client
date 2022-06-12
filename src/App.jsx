fa7be4ab64aaf697dd53d10c62e102556f081a43
Fatkhulloh <eternallfame@gmail.com>
2022-06-12 14:38:42 +0500

    first table

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [orders, setOrders] = useState()
+
+  useEffect(() =>{
+    fetch('http://localhost:9000/allorders', {method: 'GET'})
+			.then((response) => response.json())
+			.then((data) => setOrders(data))
+			.catch((err) => console.log(err))
+  },[])
+
+  return (
+    <>
+      <table>
+  <thead>
+    <tr>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Job Title</th>
+      <th>Twitter</th>
+    </tr>
+  </thead>
+  <tbody>
+    {
+      orders && orders?.length && orders.map(o => (
+        <tr>
+          <td key={o[0]} data-column="First Name">{o[1]}</td>
+          <td key={o[0]} data-column="Last Name">{o[2]}</td>
+          <td key={o[0]} data-column="Job Title">{o[3]}</td>
+          <td key={o[0]} data-column="Twitter">{o[4]}</td>
+        </tr>
+      ))
+    }    
+  </tbody>
+</table>
+    </>
+  )
+}
+
+export default App
